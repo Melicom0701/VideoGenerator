a6daf73c189f1b85673fd6750c8585e4dfed406d
melicom <melicom0701@gmail.com>
2024-02-22 15:49:09 +0700

    turn on cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const appRoutes = require('./src/routes')
 const app = express();
 const port =  process.env.PORT;
+var cors = require('cors')
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", appRoutes);
